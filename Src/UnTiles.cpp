db2414b141d6df08fcb338291aa8cdcc16652cf6
Andrew Lamontagne <metallicafan212@gmail.com>
2023-06-07 20:22:43 -0700

    Again force tiles to have 0.5f Z when < 8.0f (Hacking HUD/Screen tiles)

--- Src/UnTiles.cpp
+++ Src/UnTiles.cpp
@@ -119,8 +119,8 @@ void UD3D11RenderDevice::DrawTile(FSceneNode* Frame, FTextureInfo& Info, FLOAT X
 		if ((Z >= 0.5f) && (Z < 8.0f))
 		{
 			// Metallicafan212:	TODO! There's been some glitchyness due to actor triangles drawing through hud elements, so forcing 0.5 might be needed, or maybe requesting near z range instead
-			//Z = 0.5f;
-			SetProjectionStateNoCheck(false);
+			Z = 0.5f;
+			//SetProjectionStateNoCheck(false);
 			//Z = (((Z - 0.5f) / 7.5f) * 2.0f) + 2.0f; 
 		}
 	}
