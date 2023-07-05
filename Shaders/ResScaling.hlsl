c3243fd153d99c93967cae2e3b73e282d3d2fc01
Andrew Lamontagne <metallicafan212@gmail.com>
2023-07-04 18:36:34 -0700

    Fix blurryness at ResolutionScale = 1.0

--- Shaders/ResScaling.hlsl
+++ Shaders/ResScaling.hlsl
@@ -59,19 +59,29 @@ PSInput VertShader(VSInput input)
 
 float4 PxShader(PSInput input) : SV_TARGET
 {
-	// Metallicafan212:	Try this
-	//					This is the algorithm from here: https://www.shadertoy.com/view/XsfGDn
-	uint SW, SH;
-	Screen.GetDimensions(SW, SH);
+	float3 TexColor;
 	
-	float2 UV	= input.uv;
-	UV 			= UV * SW + 0.5;
-	float2	iUV	= floor(UV);
-	float2 	fUV	= frac(UV);
-	UV			= iUV + fUV * fUV * (3.0 - 2.0 * fUV);
-	UV			= (UV - 0.5) / SW;
-	
-	float3	TexColor = Screen.SampleBias(ScreenState, UV, 0.0f).xyz;
+	if(ResolutionScale == 1.0)
+	{
+		// Metallicafan212:	Do normal sampling
+		TexColor = Screen.SampleBias(ScreenState, input.uv, 0.0f).xyz;
+	}
+	else
+	{
+		// Metallicafan212:	Try this
+		//					This is the algorithm from here: https://www.shadertoy.com/view/XsfGDn
+		uint SW, SH;
+		Screen.GetDimensions(SW, SH);
+		
+		float2 UV	= input.uv;
+		UV 			= UV * SW + 0.5;
+		float2	iUV	= floor(UV);
+		float2 	fUV	= frac(UV);
+		UV			= iUV + fUV * fUV * (3.0 - 2.0 * fUV);
+		UV			= (UV - 0.5) / SW;
+		
+		TexColor = Screen.SampleBias(ScreenState, UV, 0.0f).xyz;
+	}
 	
 	// Metallicafan212:	Gamma correct it
 	float OverGamma = 1.0f / Gamma;
